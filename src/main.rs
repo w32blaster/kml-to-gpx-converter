c9da670fd9331a00a37968056235d1d5cb5aae76
Ilja Hamalainen <ilja.hamalainen@infinityworks.com>
2019-09-25 15:45:40 +0100

    Successfully parse XML

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,60 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_xml_rs;
+
+use serde_xml_rs::from_reader;
+
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
+const PATH_SOURCE_FILE: &str = "/Users/ilja.hamalainen/Downloads/DirectiosFromClarendonHotelToPrincessStreetCarPark.kml";
+
+
+#[derive(Deserialize, Debug)]
+struct KmlRoot {
+    #[serde(rename="Document")]
+    pub document: Document,
+}
+
+#[derive(Deserialize, Debug)]
+struct Document {
+    #[serde(rename="Placemark")]
+    pub placemark: Vec<Placemark>,
+    pub name: String,
+
+}
+
+#[derive(Deserialize, Debug)]
+struct Placemark {
+    name: String,
+    #[serde(rename="styleUrl")]
+    style_url: String,
+}
+
+
 fn main() {
-    println!("Hello, world! yayy");
+    println!("Let's parse the source KML file");
+
+
+    // 1. read the source file and parse it
+    let res = read_source_file();
+    let root: KmlRoot = match res {
+        Ok(c) => from_reader(c.as_bytes()).unwrap(),
+        Err(error) => {
+            panic!("Problem opening the source file: {:?}", error)
+        },
+    };
+
+    println!("{:?}", root);
+}
+
+// read the source file and returns its content as a string
+fn read_source_file() -> std::io::Result<String> {
+    let file = File::open(PATH_SOURCE_FILE).expect("Failed to open source file");
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+    Ok(contents)
 }
